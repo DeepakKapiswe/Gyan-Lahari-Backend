243c3b2d6c06754fb93a58b0a0879d2f8e9ce9c2
deepakkapiswe <deepakkapiswe@gmail.com>
2020-08-14 09:01:48 +0530

    i#2: Modified to accecpt postgres conf file
    Signed-off-by: deepakkapiswe <deepakkapiswe@gmail.com>

--- src/App.hs
+++ src/App.hs
@@ -6,17 +6,18 @@ module App where
 
 import           Data.Aeson
 import           GHC.Generics
+import           Control.Exception
+import qualified Data.ByteString.Char8 as B
 import           Network.Wai
 import           Network.Wai.Handler.Warp
--- import           Network.Wai.Handler.WarpTLS
 import           Network.HTTP.Client (Manager, newManager, defaultManagerSettings)
 import           Network.HTTP.ReverseProxy
 import           Servant
 import           System.IO
+import           System.Environment (getArgs)
 
 import           Network.Wai.Middleware.Cors
 import           Network.Wai.Middleware.AddHeaders (addHeaders)
--- import           Network.Wai.Middleware.Servant.Options
 
 import           Adapter.HTTP.Api
 import           Adapter.HTTP.Server
@@ -32,18 +33,18 @@ import qualified Database.Redis as R
 
 runApp :: IO ()
 runApp = do
+  f <- getArgs
   let port = 7000
-  let connStr = postgreSQLConnectionString $
-                  defaultConnectInfo { 
-                    connectHost = "X.X.X.X",
-                    connectPassword = "some password",
-                    connectDatabase = "some db name" 
-                    }
-
+  connStr <- B.pack <$> case f of
+        [configFilePath] -> do
+          contents <- safeLoadFile configFilePath
+          case contents of
+            Left _ -> pure ""
+            Right connInfo -> pure connInfo
+        _ -> pure ""
 
   pool <- initConnectionPool connStr
   -- redConn <- R.checkedConnect R.defaultConnectInfo
-  initDB connStr
 
   myKey <- generateKey
   let jwtCfg = defaultJWTSettings myKey
@@ -53,4 +54,8 @@ runApp = do
         { cookieSameSite = SameSiteStrict}
   run 7000 $
     serveWithContext api ctx (server pool cookieSettings jwtCfg)
-    
+
+-- | Takes a FilePath and returns either its contents if file is present
+-- or an IOException if not
+safeLoadFile :: FilePath -> IO (Either IOException String)
+safeLoadFile f = (Right <$> readFile f) `catch` (\ e -> pure (Left e) )
